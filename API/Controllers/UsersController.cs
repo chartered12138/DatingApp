b8aa07f5168000fa49ebcb1ebc2ec0bd16183bd2
Shaoquan Qin <arthur12138@hotmail.com>
2021-11-21 23:42:26 -0600

    End of section 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
 using API.Extensions;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -33,8 +34,15 @@ namespace API.Controllers
 
         [HttpGet]
 
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
-            var users = await _userRepository.GetMembersAsync();
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams){
+
+            var user=await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername=user.UserName;
+            if(string.IsNullOrEmpty(userParams.Gender)) 
+                userParams.Gender=user.Gender=="male"?"female":"male";
+            var users = await _userRepository.GetMembersAsync(userParams);
+            Response.AddPaginationHeader(users.CurrentPage,users.PageSize,
+                users.TotalCount,users.TotalPages);
            
             return Ok(users);
             
